bfa71cb074a3c012ebe1abf3ab11573f1663ab5d
Matae300 <150396856+Matae300@users.noreply.github.com>
2024-05-24 15:31:51 -0400

    created models, for Users, Posts, Comments, and votes

---
+++ app/db/__init__.py
@@ -0,0 +1,15 @@
+from os import getenv
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Print the DB_URL to verify it's loaded correctly
+print(getenv('DB_URL'))
+
+# connect to database using env variable
+engine = create_engine(getenv('DB_URL'), echo=True, pool_size=20, max_overflow=0)
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
