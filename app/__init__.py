71cb482ec6e7ec35b2921643665deaa5707ec6f6
Matae300 <150396856+Matae300@users.noreply.github.com>
2024-05-25 15:11:36 -0400

    formatteddates on posts and comments

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,40 @@
+import os
 from flask import Flask
 from app.routes import home, dashboard
+from app.utils import filters
+from app.db import init_db
+from dotenv import load_dotenv
+
+# Load environment variables from a .env file
+load_dotenv()
 
 def create_app(test_config=None):
-  # set up app config
-  app = Flask(__name__, static_url_path='/')
-  app.url_map.strict_slashes = False
-  app.config.from_mapping(
-    SECRET_KEY='super_secret_key'
-  )
+    # Set up app config
+    app = Flask(__name__, static_url_path='/')
+    app.url_map.strict_slashes = False
+    app.jinja_env.filters['format_url'] = filters.format_url
+    app.jinja_env.filters['format_date'] = filters.format_date
+    app.jinja_env.filters['format_plural'] = filters.format_plural
+    
+    # Load default configuration
+    app.config.from_mapping(
+        SECRET_KEY=os.getenv('SECRET_KEY', 'dev_secret_key')  # Use a default secret key if not set in env
+    )
 
-  @app.route('/hello')
-  def hello():
-    return 'hello world'
-  
-  # register routes
-  app.register_blueprint(home)
-  app.register_blueprint(dashboard)
-  return app
+    if test_config is not None:
+        # Load the test config if passed in
+        app.config.update(test_config)
+    
+    # Simple route for testing
+    @app.route('/hello')
+    def hello():
+        return 'hello world'
+    
+    # Register blueprints
+    app.register_blueprint(home)
+    app.register_blueprint(dashboard)
+
+    # Initialize the database
+    init_db(app)
+
+    return app
--- app/db/__init__.py
+++ app/db/__init__.py
@@ -3,13 +3,34 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
+from flask import g, Flask
 
+# Load environment variables from a .env file
 load_dotenv()
 
 # Print the DB_URL to verify it's loaded correctly
-print(getenv('DB_URL'))
+db_url = getenv('DB_URL')
+print(f'Database URL: {db_url}')
 
-# connect to database using env variable
-engine = create_engine(getenv('DB_URL'), echo=True, pool_size=20, max_overflow=0)
+# Connect to database using env variable
+engine = create_engine(db_url, echo=True, pool_size=20, max_overflow=0)
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
+
+def init_db(app):
+    """Initialize the database."""
+    Base.metadata.create_all(engine)
+    app.teardown_appcontext(close_db)
+
+def get_db():
+    """Get a database session."""
+    if 'db' not in g:
+        # Store db connection in app context
+        g.db = Session()
+    return g.db
+
+def close_db(e=None):
+    """Close the database session."""
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
